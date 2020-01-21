f8ea0afe6f2a54715e22e7fb0c5f6a04ecb58372
redfoxgamedev <devryzhov@gmail.com>
2020-01-20 17:16:27 -0800

    - Described the algorithm - Added the test GET request

--- NPCSubmission.js
+++ NPCSubmission.js
@@ -1,6 +1,23 @@
 function OnSubmitButtonClick()
 {
-    document.getElementById("OutputText").textContent = "Submitted!";
+    http = new XMLHttpRequest();
+    url = "https://jsonplaceholder.typicode.com/posts";
+    http.open("GET", url);
+    http.send();
+
+    http.onreadystatechange = (e) =>
+    {
+        document.getElementById("OutputText").textContent = http.responseText;
+    }
+
+    //1. Get information from the fields
+    //2. Validate information
+    //3. If validated, generate a request (create a json, etc.)
+    //4. POST request and handle success and failure
+    //5. That's it.
+    //The rest is just design
+
+    //document.getElementById("OutputText").textContent = "Submitted!";
 }
 
 function OnClearButtonClick()
